b149c367b4708a44bfb62799ed878986753697db
Abubakar1010 <abubakar850772@gmail.com>
2024-04-27 20:26:42 +0600

    finished project setup with react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Project Setup Finished</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from '@material-tailwind/react'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<App />
+  }
+
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider>
+    <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>,
+)
