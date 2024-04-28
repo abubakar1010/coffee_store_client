ccfdf809e659f1c67bb5107011145d465690f8ad
Abubakar1010 <abubakar850772@gmail.com>
2024-04-28 23:55:26 +0600

    finished delete operation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,19 @@
 import { Button } from "@material-tailwind/react";
-
+import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
 const Card = ({coffee}) => {
-    const {_id, name, chef, supplier, taste, category, details,photo} = coffee
+    const {_id, name, chef, supplier, photo} = coffee
 
     const handleDelete = (id) => {
         console.log(id);
+        fetch(`http://localhost:5000/coffee/${_id}`,{
+            method: 'DELETE'
+        })
+        .then( res => res.json())
+        .then( data => {
+            console.log(data)
+        })
     }
     return (
         <div className=" flex items-center gap-12">
@@ -15,12 +23,19 @@ const Card = ({coffee}) => {
                 <p>{chef}</p>
                 <p>{supplier}</p>
                 <div className=" flex gap-5 mt-4">
-                    <Button>EDIT</Button>
+                    
                     <Button onClick={() => handleDelete(_id)}>DELETE</Button>
+                    <Link to={`/updateCoffee/${_id}`}>
+                    <Button>EDIT</Button>
+                    </Link>
                 </div>
             </div>
         </div>
     );
 };
+
+Card.propTypes = {
+    coffee: PropTypes.object
+}
 
 export default Card;
